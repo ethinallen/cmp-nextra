86539d288b7e170fb0f32cfc3756c682be9d16cd
Drew <34930628+ethinallen@users.noreply.github.com>
2024-09-16 09:21:32 -0400

    update title, description, and image for text previews

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,14 @@ import { GoogleAnalytics } from "nextjs-google-analytics";
 const App = ({ Component, pageProps }) => {
   return (
     <>
+        <Head>
+          <meta property="og:title" content="Critical Mass Princeton" />
+          <meta property="og:description" content="Join the community bicycle ride in Princeton. Open to all!" />
+          <meta property="og:image" content="https://cdn.andrewemery.io/bkg.png" />
+          <meta property="og:url" content="https://cmp.bike" />
+          <meta property="og:type" content="website" />
+          {/* Other head elements */}
+        </Head>
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </>
