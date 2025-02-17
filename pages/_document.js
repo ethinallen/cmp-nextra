2f4b7bec1e96090de85ae1eaad87cdf7e40a39f9
Drew <34930628+ethinallen@users.noreply.github.com>
2025-02-17 13:08:50 -0500

    change og image #hopefully; add location to index

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
             <title>Critical Mass Princeton</title>
             <meta property="og:title" content="Critical Mass Princeton" />
             <meta property="og:description" content="Join the community bicycle ride in Princeton. Open to all!" />
-            <meta property="og:image" content="https://cdn.andrewemery.io/bkg.png" />
+            <meta property="og:image" content="https://cdn.andrewemery.io/cruiser_image.webp" />
             <meta property="og:url" content="https://cmp.bike" />
             <meta property="og:type" content="website" />
           {/* Other head elements */}
